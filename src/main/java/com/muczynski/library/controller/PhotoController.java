b921a056ddf1fcda4a25b4d01d216450a6bf94b8
John Muczynski <johnstosh@gmail.com>
2025-10-29 17:07:58 +0000

    open some permissions

--- src/main/java/com/muczynski/library/controller/PhotoController.java
+++ src/main/java/com/muczynski/library/controller/PhotoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,6 +21,7 @@ public class PhotoController {
     @Autowired
     private PhotoService photoService;
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/{id}/image")
     public ResponseEntity<byte[]> getImage(@PathVariable Long id) {
         try {
@@ -38,6 +40,7 @@ public class PhotoController {
         }
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/{id}/thumbnail")
     public ResponseEntity<byte[]> getThumbnail(@PathVariable Long id, @RequestParam Integer width) {
         try {
--- src/main/java/com/muczynski/library/controller/SearchController.java
+++ src/main/java/com/muczynski/library/controller/SearchController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +25,7 @@ public class SearchController {
     private SearchService searchService;
 
     @GetMapping
+    @PreAuthorize("permitAll()")
     public ResponseEntity<?> search(@RequestParam String query, @RequestParam int page, @RequestParam int size) {
         try {
             Map<String, Object> results = searchService.search(query, page, size);
